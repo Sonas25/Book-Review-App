4bb1845ac4f55a7fb52d6def8e95a8e3a6c16c1e
LILIYA SUJO <liliyasujo@gmail.com>
2021-09-25 16:39:55 +0530

    third commit

--- lib/data/bookshelper.dart
+++ lib/data/bookshelper.dart
@@ -2,20 +2,20 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:async';
-import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../favoriteScreen.dart';
 import 'book.dart';
 import 'package:book_review_app/env/key.dart' as config;
 
 class BooksHelper {
-  final String urlKey = '&key=${config.apiKey}';
-  final String urlQuery = 'volumes?q=';
-  final String urlBase = 'https://www.googleapis.com/books/v1/';
+  // final String urlKey = '&key=${config.apiKey}';
+  // final String urlQuery = 'volumes?q=';
+  // final String urlBase = 'https://www.googleapis.com/books/v1/';
 
   Future<List<dynamic>?> getBooks(String query) async {
-    final String url = urlBase + urlQuery + query;
-    Response result = await http.get(Uri.parse(url));
+    // final String url = urlBase + urlQuery + query;
+    http.Response result = await http.get(Uri.parse(
+        'https://www.googleapis.com/books/v1/volumes?q=$query+inauthor:keyes&key=${config.apiKey}'));
     if (result.statusCode == 200) {
       final jsonResponse = json.decode(result.body);
       final booksMap = jsonResponse['items'];
